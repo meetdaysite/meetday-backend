2c0f4713effd036a08591f52c354e918d20fb2f6
Adrita Guha <adrita.g.2005@gmail.com>
2026-08-20 20:20:14 +0530

    migration: reset chats on deployment

---
+++ prisma/migrations/20260820202000_reset_chats/migration.sql
@@ -0,0 +1,5 @@
+-- Clear all sponsorship interests (cascades to messages and deals)
+TRUNCATE TABLE sponsorship_interests CASCADE;
+
+-- Clear all general Meetday support chat threads (cascades to messages)
+TRUNCATE TABLE "MeetdayChatThread" CASCADE;
